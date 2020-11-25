47055875eb9c9911bef407a46b259b1289a09c44
Crowdbotics <team@crowdbotics.com>
2020-11-25 08:45:31 +0000

    Update models

--- msm_web_2511_dev_15674/wsgi.py
+++ msm_web_2511_dev_15674/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'msm_web_2511_dev_15674.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "msm_web_2511_dev_15674.settings")
 
 application = get_wsgi_application()
